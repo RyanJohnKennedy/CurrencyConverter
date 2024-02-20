322f470696a8a93ace777575b01c7dc1a109b7c1
RyanJohnKennedy <Ryan.Kennedy.John@gmail.com>
2024-02-20 19:11:16 +0200

    Update

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -13,7 +12,7 @@ public class CurrencyConverter {
 
     public static void main(String[] args) throws IOException {
 
-        HashMap<Integer, String> currencyCodes = new HashMap<Integer, String>();
+        HashMap<Integer, String> currencyCodes = new HashMap<>();
 
         // Add currency codes
         currencyCodes.put(1, "USD");
@@ -82,7 +81,7 @@ public class CurrencyConverter {
         if(responseCode == HttpURLConnection.HTTP_OK){ // success
             BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
             String inputLine;
-            StringBuffer response = new StringBuffer();
+            StringBuilder response = new StringBuilder();
 
             while((inputLine = in.readLine()) != null){
                 response.append(inputLine);
